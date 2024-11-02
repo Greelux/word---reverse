1c068dad36bd2005cd2ee0791810d2268f82d3e9
Greelux <154830952+Greelux@users.noreply.github.com>
2024-11-02 22:00:58 +0200

    Добавьте файлы проекта.

---
+++ word - reverse/word - reverse.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    setlocale(LC_ALL, "");
+
+    string word;
+
+    cout << "Введіть слово, яке потрібно записати навпаки: " << endl;
+    cin >> word;
+
+    reverse(word.begin(), word.end());
+
+    ofstream file("output.txt"); 
+    if (file.is_open()) {
+        file << word;
+        file.close();
+        cout << "Слово записано у файл 'output.txt' у зворотному порядку." << endl;
+    }
+    else {
+        cout << "Невдалося відкрити файл:(" << endl;
+    }
+
+    return 0;
+}
+
